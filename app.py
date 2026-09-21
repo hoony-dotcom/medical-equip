29b3c62b64909dbfbf6b0b0149422826c7997ee8
hoony-dotcom <c2619350@gmail.com>
2026-09-22 08:24:14 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -255,8 +255,8 @@ st.sidebar.markdown(
         <span style="font-size: 0.95rem; font-weight: bold; color: #111111;">🔗 의용공학팀 개발 앱</span><br>
         <div style="margin-top: 5px; font-size: 0.88rem; line-height: 1.4;">
             1. <a href="https://buly.kr/DEbvdwF" target="_blank" style="color: #2980B9; text-decoration: none;">의료장비 투자집행 계획 실적</a><br>
-            2. <a href="https://buly.kr/7mERs3u" target="_blank" style="color: #2980B9; text-decoration: none;">의료장비 현황 바로가기</a><br>
-            3. <a href="https://buly.kr/uWvRbg" target="_blank" style="color: #2980B9; text-decoration: none;">심평원 신고 장비 상세현황</a>
+            2. <a href="https://buly.kr/7mERs3u" target="_blank" style="color: #2980B9; text-decoration: none;">인하대병원 의료장비 보유 현황</a><br>
+            3. <a href="https://buly.kr/uWvRbg" target="_blank" style="color: #2980B9; text-decoration: none;">건강보험심사평가원 의료장비 상세현황 조회</a>
         </div>
     </div>
     """,
